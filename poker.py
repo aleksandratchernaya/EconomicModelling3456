b7460ea41178129e3a8105297dd45e95f58e8051
sashatchernaya <sasha.tchernaya1@gmail.com>
2024-04-05 10:51:59 +0200

    Initial commit

---
+++ poker.py
@@ -0,0 +1,89 @@
+import random
+
+
+class Card:
+    SUITS = ["♣", "♦", "♥", "♠"]
+    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+    def _init_(self, rank, suit):
+        if rank not in self.RANKS:
+            raise Exception(f"Invalid rank, must be one of {self.RANKS}")
+        if suit not in self.SUITS:
+            raise Exception(f"Invalid suit, must be one of {self.SUITS}")
+        self._rank = rank
+        self._suit = suit
+
+    @property
+    def suit(self):
+        return self._suit
+
+    @property
+    def rank(self):
+        return self._rank
+
+    def _str_(self):
+        return f"{self.rank}{self.suit}"
+
+    def _repr_(self):
+        return self._str_()
+
+
+class Deck:
+    def _init_(self):
+        cards = []
+        # iterate over all ranks and suits, create a card and add it to the list
+        for rank in Card.RANKS:
+            for suit in Card.SUITS:
+                card = Card(rank, suit)
+                cards.append(card)
+        self._cards = tuple(cards)
+
+    @property
+    def cards(self):
+        return self._cards
+
+    def _str_(self):
+        return str(self.cards)
+
+    def shuffle(self):
+        # shuffles the cards in the deck
+        cards = list(self.cards)
+        random.shuffle(cards)
+        self._cards = tuple(cards)
+
+class Hand:
+    def _init_(self, deck):
+        # deck is shuffled before
+        cards = []
+        for i in range(5):
+            cards.append(deck.cards[i])
+        self._cards = tuple(cards)
+
+    def _str_(self):
+        return str(self._cards)
+
+    @property
+    def is_flush(self):
+        suit = self._cards[0].suit
+        for i in range(1, 5):
+            if self._cards[i].suit != suit:
+                return False
+
+        return True
+
+
+tries = 10
+i = 0
+while True:
+    i = i + 1
+    d = Deck()
+    d.shuffle()
+    hand = Hand(d)
+    if hand.is_flush:
+        tries -=1
+
+    if tries==0:
+        break
+
+probability = tries/i * 100
+print(f"The odds of getting a flush are {probability}%")
